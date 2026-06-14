f940a1fcc9136bda75e7535132cf6385d86ff3bd
Ariyan-real <azoainariyan2006@gmail.com>
2026-06-14 10:49:56 +0600

    First version of my love website

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const startDate = new Date("2025-14-06");
 
+const today = new Date();
+
+const difference =
+today - startDate;
+
+const days =
+Math.floor(
+difference /
+(1000*60*60*24)
+);
+
+document.getElementById(
+"days"
+).innerText = days;
+
+function showMessage(){
+
+document.getElementById(
+"hiddenMessage"
+).innerText =
+
+"I choose you. Every day. Every time. ❤️";
+
+}
